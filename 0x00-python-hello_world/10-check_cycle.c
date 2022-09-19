9540aea94c6f0345aa24f058008fa477f6444543
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-09-19 21:07:30 +0300

    match nodes

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,1 +1,31 @@
+#include "lists.h"
+#include <stdio.h>
 
+/**
+ * check_cycle - checks if singly linked list is a cycle
+ * Return: 0 if no cycle, 1 is yes
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *current = list;
+	listint_t *temp = list;
+
+	if (!list)
+		return (0);
+
+	while (1)
+	{
+		/*traverse through nodes as long as linked list node exists*/
+		if (current->next != NULL && current->next->next != NULL)
+		{
+			current = current->next->next;
+			temp = temp->next;
+
+			if (current == temp) /*if nodes match, cycle found*/
+				return (1);
+		}
+		else
+			return (0);
+	}
+
+}
