3a59e188edd6440e2c0e64db7122e34b929dbeea
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-09-20 22:11:00 +0300

    Update 13-insert_number.c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,1 +1,33 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	listint_t *head;
+
+	head = NULL;
+	add_nodeint_end(&head, -27);
+	add_nodeint_end(&head, 1);
+	add_nodeint_end(&head, 2);
+
+	print_listint(head);
+
+	printf("-----------------\n");
+
+	insert_node(&head, -100);
+	insert_node(&head, -10);
+	insert_node(&head, -11);
+
+	print_listint(head);
+
+	free_listint(head);
+
+	return (0);
+}
