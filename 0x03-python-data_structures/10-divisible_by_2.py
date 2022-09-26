5a30a3fc1b45a7eb333644779bd592106ec579fc
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-09-26 20:26:51 +0300

    Update 10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     if my_list is not None:
         values = []
-        for i in range(len(my_list)):
+        for i in my_list:
             if i % 2 is 0:
                 values.append(True)
             else:
