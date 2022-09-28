acc400d82a592bab700abe5f4405e7492e27edbb
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-09-28 20:50:05 +0300

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,7 @@
     A function to get a square of a matrix elements
 '''
 def square_matrix_simple(matrix=[]):
-    if matrix !=0:
+    if matrix is not None:
         new = []
         for rows in matrix:
             new.append(list(map(lambda x: x**2, rows)))
