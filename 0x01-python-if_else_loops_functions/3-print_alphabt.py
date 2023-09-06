9a6bf6cae0a3b0c6beb18fe844c38028767163e6
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2023-09-06 05:53:34 +0300

    Update 3-print_alphabt

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# Author - George KYAMBADDE
-for letter in range(97, 123):
-    if chr(letter) != 'q' and chr(letter) != 'e':
-        print("{}".format(chr(letter)), end="")
+for i in range(ord('a'), ord('z')+1):
+    if chr(i) not in "qe":
+        print("{}".format(chr(i)), end="")
