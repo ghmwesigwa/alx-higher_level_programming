b133c23c274e7845c2fc3beae00ab1f164ede90c
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2023-09-07 14:43:05 +0300

    Update 2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
+import sys
+
 if __name__ == "__main__":
-    from sys import argv
-argc = len(argv)
-if argc < 2:
-    print("{} arguments.".format(argc - 1))
-else:
-    if argc == 2:
-        print("{} argument:".format(argc - 1))
-    else:
-        print("{} arguments:".format(argc - 1))
-    for n in range(1, argc):
-        print("{}: {}".format(n, argv[n]))
+    argv = sys.argv  # Get the list of command-line arguments
+
+    # Calculate the number of arguments
+    num_args = len(argv) - 1  # Subtract 1 to exclude the script name
+
+    # Print the number of arguments and the appropriate label (argument or arguments)
+    print("{} {}:".format(num_args, "argument" if num_args == 1 else "arguments"))
+
+    # Print each argument with its position
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
