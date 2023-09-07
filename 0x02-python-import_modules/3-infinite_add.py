93b7dbf0a4655bde82fea7f8d3cd5d1a68f7f246
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2023-09-07 14:49:51 +0300

    Update 3-infinite_add.

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+if __name__ == '__main__':
     import sys
-    result = 0
-    for arg in sys.argv:
-        if arg != sys.argv[0]:
-            result += int(arg)
-    print(result)
+    Sum = 0
+    num = sys.argv
+    if len(num) > 1:
+        for i in range(1, len(num)):
+            Sum += int(num[i])
+    print('{:d}'.format(Sum))
